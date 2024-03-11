7d4b41889e0a941869a8c97a1817030855a675da
Val Lex <102872929+val-Lex@users.noreply.github.com>
2024-03-11 10:29:37 +0700

    Add files via upload

---
+++ FNaF Panorama/Assets/Panorama/Panorama.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Panorama : MonoBehaviour
+{
+    private Shader _shader;
+    protected Material _mat;
+
+    private void Awake()
+    {
+        _shader = Shader.Find("Custom/Panorama");
+        _mat = new Material(_shader);
+        Init();
+    }
+
+    protected virtual void Init()
+    {
+
+    }
+
+    private void Update()
+    {
+        OnUpdate();
+    }
+
+    protected virtual void OnUpdate()
+    {
+
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, _mat);
+    }
+}
---
+++ FNaF Panorama/Assets/Script/CameraMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public bool MoveLeft;
+    public bool MoveRight;
+
+    public float moveDir;
+
+    void Update()
+    {
+        transform.position += new Vector3(moveDir, 0f, 0f);
+        if (MoveLeft)
+        {
+            if(transform.position.x > -2.25f)
+            {
+                moveDir = -0.2f;
+            }
+            else
+            {
+                moveDir = 0f;
+                transform.position = new Vector3(-2.25f, transform.position.y, transform.position.z);
+            }
+        }
+        else if (MoveRight)
+        {
+            if (transform.position.x < 2.25f)
+            {
+                moveDir = 0.2f;
+            }
+            else
+            {
+                moveDir = 0f;
+                transform.position = new Vector3(2.25f, transform.position.y, transform.position.z);
+            }
+        }
+        else
+        {
+            moveDir = 0f;
+        }
+    }
+    public void StopLeft()
+    {
+        MoveLeft = false;
+    }
+    public void moveLeft()
+    {
+        MoveLeft = true;
+    }
+    public void StopRight()
+    {
+        MoveRight = false;
+    }
+    public void moveRight()
+    {
+        MoveRight = true;
+    }
+}
